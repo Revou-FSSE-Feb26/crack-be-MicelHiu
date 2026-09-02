b8cad2f057ec73dfa42531c1d694fe5229c8f08e
MicelHiu <micelasatu@gmail.com>
2026-09-03 01:11:05 +0700

    feat: repository for create and update carts

--- src/carts/carts.repository.ts
+++ src/carts/carts.repository.ts
@@ -1,5 +1,7 @@
 import { Injectable } from "@nestjs/common";
 import { PrismaService } from "src/prisma/prisma.service";
+import { CreateCartDto } from "./dto/create-cart.dto";
+import { UpdateCartDto } from "./dto/update-cart.dto";
 
 @Injectable()
 export class CartsRepository {
@@ -16,5 +18,58 @@ export class CartsRepository {
         });
     }
 
-    
+    getRoomById(room_id: string) {
+        return this.prisma.rooms.findUnique({
+            where: {id: room_id},
+            select: {
+                name: true, 
+                price: true, 
+                type: true, 
+                image: true
+            }
+        });
+    }
+
+    createCart(dto: CreateCartDto, newStock: number) {
+        this.prisma.carts.create({
+            data: dto,
+            include: {
+                rooms: {
+                    select: {
+                        name: true,
+                        price: true,
+                        type: true,
+                        image: true,
+                        stock: true,
+                    }
+                }
+            }
+        }),
+        this.prisma.rooms.update({
+            where: {
+                id: dto.room_id,
+            },
+            data: {
+                stock: newStock,
+            }
+        });
+    }
+
+    updateCart(dto: UpdateCartDto, id: string, newStock: number, room_id: string) {
+        this.prisma.carts.update({
+            where: { id },
+            data: dto,
+            include: {
+                rooms: { select: {name: true, stock: true} }
+            }
+        }),
+        this.prisma.rooms.update({
+            where: {
+                id: room_id,
+            },
+            data: {
+                stock: newStock,
+            }
+        });
+    }
 }
--- src/carts/dto/update-cart.dto.ts
+++ src/carts/dto/update-cart.dto.ts
@@ -1,16 +1,16 @@
 import { IsDate, IsDecimal, IsNumber, IsString, IsUUID, Matches, Min } from "class-validator"
 import { Decimal } from "generated/prisma/internal/prismaNamespace";
 
-export class CreateCartDto {
+export class UpdateCartDto {
     @IsUUID()
     user_id!: string;
 
     @IsString()
-    room_id!: string;
+    room_id?: string;
 
     @IsNumber()
     @Min(1)
-    quantity!: number;
+    quantity?: number;
 
     @IsUUID()
     discount_id?: string;      
@@ -19,20 +19,20 @@ export class CreateCartDto {
     discount_value?: Decimal;
     
     @IsDate()
-    date_play!: Date;
+    date_play?: Date;
 
     @IsString()
     @Matches(/^([01]\d|2[0-3]):([0-5]\d)(:([0-5]\d))?$/, {
         message: 'time must be in format HH:mm or HH:mm:ss',
     })
-    time_start!: string;
+    time_start?: string;
     
     @IsString()
     @Matches(/^([01]\d|2[0-3]):([0-5]\d)(:([0-5]\d))?$/, {
         message: 'time must be in format HH:mm or HH:mm:ss',
     })
-    time_end!: string;
+    time_end?: string;
 
     @IsDecimal()
-    total_price!: Decimal;     
+    total_price?: Decimal;     
 }
